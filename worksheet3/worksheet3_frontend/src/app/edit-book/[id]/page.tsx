8901eeb369085fd9eecdcb089fe4a6718539bc9e
alexaalvaran <130896929+alexaalvaran@users.noreply.github.com>
2024-08-03 17:23:36 +1200

    complete front-end app

---
+++ worksheet3/worksheet3_frontend/src/app/edit-book/[id]/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import React from "react";
+import UpdateBookInfo from "../../../components/UpdateBookInfo";
+
+export default function ShowBook(){
+    return(
+        <UpdateBookInfo/>
+    )
+}
---
+++ worksheet3/worksheet3_frontend/src/app/show-book/[id]/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import React from "react";
+import ShowBookDetails from "../../../components/ShowBookDetails";
+
+export default function ShowBook(){
+    return (
+        <ShowBookDetails/>
+    )
+}
