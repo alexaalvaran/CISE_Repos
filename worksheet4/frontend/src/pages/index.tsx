e509eddb776852dda2ebb918c5cc6e17d3f98cda
alexaalvaran <130896929+alexaalvaran@users.noreply.github.com>
2024-08-11 20:19:22 +1200

    add code to files under src/parges

--- worksheet4/frontend/src/pages/index.tsx
+++ worksheet4/frontend/src/pages/index.tsx
@@ -1,4 +1,4 @@
-export default function Hpme(){
+export default function Home(){
     return(
         <div className="container">
             <h1>Software Practice Emperical Evidence Database (SPEED)</h1>
